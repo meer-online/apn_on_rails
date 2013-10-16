6a8d3147c98672ca63eab9dd00fea9214847f209
Morris Singer <morrissinger@gmail.com>
2013-10-16 15:42:33 -0400

    debug

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -53,8 +53,8 @@ class APN::App < APN::Base
               puts "-----------------\n"
 
               puts conn.write(noty.message_for_sending)
-              puts "SLEEPING 0.0675"
-              sleep 0.0675
+              puts "SLEEPING 0.03375"
+              sleep 0.03375
               puts "^^^^^^^^^^^^^^^^^\n"
               noty.sent_at = Time.now
               noty.save
